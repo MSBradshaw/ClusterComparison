34be92280f7c3a7451293b902589efc9ad0a386d
Michael Bradshaw <michaelscottbradshaw@gmail.com>
2021-06-03 14:28:22 -0600

    added scripts for 4 cluster algorithms

---
+++ Algorithms/belief.py
@@ -0,0 +1,32 @@
+from cdlib import algorithms
+import networkx as nx
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+
+    parser.add_argument('--edgelist',
+                        dest='edgelist',
+                        required=True,
+                        help='tab separated edge list')
+
+    parser.add_argument('--output',
+                        dest='output',
+                        required=True,
+                        help='name of file to save the community to')
+
+    args = parser.parse_args()
+    return args
+
+
+args = get_args()
+
+G = nx.read_edgelist(args.edgelist)
+coms = algorithms.belief(G)
+
+with open(args.output, 'w') as outfile:
+    for i, com in enumerate(coms):
+        outfile.write('\t'.join([str(i)] + com))
+        outfile.write('\n')
